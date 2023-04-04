c7482e1ecae8675b12d6af0daaefe86225c5b05a
okey-js <kokehinc@gmail.com>
2023-04-04 23:37:41 +0100

    done some DOM manipulation

---
+++ script.js
@@ -0,0 +1,52 @@
+const username = document.getElementById('username');
+
+// username.addEventListener('onkeyup', handleKeyboardInput)
+// username.onkeyup = handleKeyboardInput;
+username.onkeydown = submit;
+function submit(e) {
+    if (e.key == 'Enter' || e.code == 13) {
+        e.preventDefault()
+    handleKeyboardInput()
+    }
+}
+function handleKeyboardInput() {
+    openAccountModal()
+}
+
+const body = document.getElementById('body');
+const firstContainer = document.getElementById('container');
+const firstHeader = document.getElementById('header')
+const inputOne = document.getElementById('username');
+const firstFooter = document.getElementById('footer');
+
+const removeOldHTML = () => {
+    firstContainer.remove();
+    firstHeader.remove();
+}
+const newHTML = () => {
+    const secondContainer = document.createElement('div');
+    const addToLibraryBtn = document.createElement('div');
+    const secondHeader = document.createElement('h1');
+    
+    secondContainer.setAttribute('id', 'container2');
+    addToLibraryBtn.classList.add('addToLib','big','white');
+    addToLibraryBtn.textContent = '+ Add Book';
+    secondContainer.classList.add('container2');
+    secondHeader.setAttribute('id', 'username2');
+    secondHeader.classList.add('header2','white');
+
+    firstFooter.before(secondContainer);
+    secondContainer.before(secondHeader);
+    secondContainer.appendChild(addToLibraryBtn);
+
+
+    secondHeader.textContent = "My Library";
+    // const usernameValue = document.getElementById('username').value;
+    // document.getElementById('username2').innerHTML = usernameValue;
+    // inputOne.value
+}
+
+const openAccountModal = () => {
+    removeOldHTML();
+    newHTML();
+}
