ec236852d68d124bbf87d5eec4e0e19c67697915
okey-js <kokehinc@gmail.com>
2023-04-07 17:37:00 +0100

    switching branchs

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
+/*class Book{
+    constructor(
+        title = 'Unknown',
+        author = 'Unkwown',
+        pages = '0',
+        status = false
+    ){
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.status = status
+    }
+}
+
+class Library {
+    constructor(){
+        this.books = [];
+    }
+    addBook(newBook){
+        if(!this.isInLibrary(newBook)){
+            this.books.push(newBook)
+        }
+    }
+    removeBook(title){
+        this.books = this.books.filter((book) => book.title !== title)
+    }
+    getBook(title){
+        return this.books.find((book) => book.title === title)
+    }
+    isInLibrary(newBook){
+        return this.books.some((book) => book.title === newBook.title)
+    }
+}*/
+
 const username = document.getElementById('username');
 
-// username.addEventListener('onkeyup', handleKeyboardInput)
-// username.onkeyup = handleKeyboardInput;
 username.onkeydown = submit;
 function submit(e) {
     if (e.key == 'Enter') {
@@ -39,12 +71,6 @@ const newHTML = () => {
     secondContainer.before(secondHeader);
     secondContainer.appendChild(addToLibraryBtn);
 
-
-    // secondHeader.textContent = "My Library";
-    // const usernameValue = document.getElementById('username').value;
-    // document.getElementById('username2').innerHTML = usernameValue;
-    // inputOne.value
-
     username.oninput = function(){
         secondHeader.innerHTML = username.value + `'s Library`;
     }();
@@ -53,4 +79,29 @@ const newHTML = () => {
 const openAccountModal = () => {
     removeOldHTML();
     newHTML();
+}
+
+
+function Book(title,author,pages,status){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
+
+const createFormModal = () => {
+    const formContainer = document.createElement('form');
+    const heading = document.createElement('h3');
+    const title = document.createElement('input');
+    const author = document.createElement('input');
+    const pages = document.createElement('input');
+    const status = document.createElement('input');
+    
+    title.setAttribute('type','text');
+    author.setAttribute('type','text');
+    pages.setAttribute('type','number');
+    status.setAttribute('type','checkbox');
+
+    formContainer.classList.add('form-container');
+    heading.classList.add('heading-h3');
 }
