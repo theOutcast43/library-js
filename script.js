583c324d204da36dc88bf23991be51099480636a
okey-js <kokehinc@gmail.com>
2023-04-08 22:45:50 +0100

    pushing forward with the DOM

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const removeOldHTML = () => {
 const newHTML = () => {
     const secondContainer = document.createElement('div');
     const addToLibraryBtn = document.createElement('div');
+    const booksGrid = document.createElement('div');
     const secondHeader = document.createElement('h1');
     
     secondContainer.setAttribute('id', 'container2');
@@ -22,20 +23,132 @@ const newHTML = () => {
     addToLibraryBtn.setAttribute('id','add-book');
     addToLibraryBtn.textContent = '+ Add Book';
     secondContainer.classList.add('container2');
+    booksGrid.setAttribute('id','booksGrid');
+    booksGrid.classList.add('booksGrid');
     secondHeader.setAttribute('id', 'username2');
     secondHeader.classList.add('header2','white');
 
     firstFooter.before(secondContainer);
     secondContainer.before(secondHeader);
     secondContainer.appendChild(addToLibraryBtn);
+    secondContainer.appendChild(booksGrid);
 
     username.oninput = function(){
         secondHeader.innerHTML = username.value + `'s Library`;
     }();
 
     addToLibraryBtn.onclick = createForm;
-}
+    function createForm() {
+        overlayModal();
+        createFormModal();
+    }
 
+    const createFormModal = () => {
+        const formContainer = document.createElement('form');
+        const div = document.createElement('div');
+        const exitButton = document.createElement('h4');
+        const heading = document.createElement('h3');
+        const title1 = document.createElement('input');
+        const author1 = document.createElement('input');
+        const pages1 = document.createElement('input');
+        const labelStatus1 = document.createElement('label');
+        const status1 = document.createElement('input');
+        const submit1 = document.createElement('button');
+        
+        exitButton.setAttribute('id','exit');
+        title1.setAttribute('id','title');
+        title1.setAttribute('type','text');
+        title1.setAttribute('placeholder','Title');
+        title1.setAttribute('oninput','');
+        title1.setAttribute('required','');
+        author1.setAttribute('id','author');
+        author1.setAttribute('type','text');
+        author1.setAttribute('placeholder','Author/Writer');
+        author1.setAttribute('oninput','');
+        pages1.setAttribute('id','pages');
+        pages1.setAttribute('type','number');
+        pages1.setAttribute('placeholder','Pages Read')
+        pages1.setAttribute('oninput','');
+        status1.setAttribute('id','status');
+        status1.setAttribute('type','checkbox');
+        status1.setAttribute('name','status');
+        submit1.setAttribute('type','submit');
+        labelStatus1.setAttribute('for','status');
+    
+        formContainer.classList.add('form-container');
+        div.classList.add('div');
+        exitButton.classList.add('exitBtn','white');
+        heading.classList.add('head-h3','white');
+        title1.classList.add('in-title');
+        author1.classList.add('in-author');
+        pages1.classList.add('in-pages');
+        status1.classList.add('in-status');
+        submit1.classList.add('in-submit','white');
+        labelStatus1.classList.add('white');
+    
+        exitButton.textContent = `x`;
+        heading.textContent = `Add Book`;
+        labelStatus1.textContent = `Have you read this book? `;
+        submit1.textContent = `Submit`;
+    
+        overlay.appendChild(formContainer);
+        formContainer.appendChild(div)
+        div.appendChild(heading);
+        div.appendChild(exitButton);
+        formContainer.appendChild(title1);
+        formContainer.appendChild(author1);
+        formContainer.appendChild(pages1);
+        formContainer.appendChild(labelStatus1);
+        formContainer.appendChild(status1);
+        formContainer.appendChild(submit1);  
+        
+        exitButton.onclick = function() {
+            formContainer.remove();
+            closeModal();
+        };
+        submit1.onclick = function(e){
+            e.preventDefault();
+            formContainer.remove();
+            createBookCard();
+            closeModal();
+        }
+        const createBookCard = () => {
+            const bookCard = document.createElement('div')
+            const title2 = document.createElement('p')
+            const author2 = document.createElement('p')
+            const pages2 = document.createElement('p')
+            const buttonGroup = document.createElement('div')
+            const readBtn = document.createElement('button')
+            const removeBtn = document.createElement('button')
+
+            bookCard.classList.add('book-card')
+            buttonGroup.classList.add('button-group')
+            readBtn.classList.add('btn')
+            removeBtn.classList.add('btn')
+            // readBtn.onclick = toggleRead
+            removeBtn.onclick = removeBook
+            removeBtn.textContent = 'Remove'
+
+            title1.oninput = function(){
+                title2.innerHTML = title1.value;
+            }();
+            author1.oninput = function(){
+                author2.innerHTML = author1.value;
+            }();
+            pages1.oninput = function(){
+                pages2.innerHTML = pages1.value + ` pages`;
+            }();
+
+            bookCard.appendChild(title2)
+            bookCard.appendChild(author2)
+            bookCard.appendChild(pages2)
+            buttonGroup.appendChild(readBtn)
+            buttonGroup.appendChild(removeBtn)
+            bookCard.appendChild(buttonGroup)
+            booksGrid.appendChild(bookCard)
+        }
+    }
+}
 const openAccountModal = () => {
     removeOldHTML();
     newHTML();
@@ -48,71 +161,13 @@ function Book(title,author,pages,status){
     this.status = status;
 }
 
-const createFormModal = () => {
-    const formContainer = document.createElement('form');
-    const div = document.createElement('div');
-    const exitButton = document.createElement('h4');
-    const heading = document.createElement('h3');
-    const title = document.createElement('input');
-    const author = document.createElement('input');
-    const pages = document.createElement('input');
-    const labelStatus = document.createElement('label');
-    const status = document.createElement('input');
-    const submit = document.createElement('button');
-    
-    exitButton.setAttribute('id','exit');
-    title.setAttribute('type','text');
-    title.setAttribute('placeholder','Title');
-    author.setAttribute('type','text');
-    author.setAttribute('placeholder','Author/Writer');
-    pages.setAttribute('type','number');
-    pages.setAttribute('placeholder','Pages Read')
-    status.setAttribute('type','checkbox');
-    status.setAttribute('name','status');
-    submit.setAttribute('type','submit');
-    labelStatus.setAttribute('for','status');
-
-    formContainer.classList.add('form-container');
-    div.classList.add('div');
-    exitButton.classList.add('exitBtn','white');
-    heading.classList.add('head-h3','white');
-    title.classList.add('in-title');
-    author.classList.add('in-author');
-    pages.classList.add('in-pages');
-    status.classList.add('in-status');
-    submit.classList.add('in-submit','white');
-    labelStatus.classList.add('white');
-
-    exitButton.textContent = `x`;
-    heading.textContent = `Add Book`;
-    labelStatus.textContent = `Have you read this book? `;
-    submit.textContent = `Submit`;
-
-    overlay.appendChild(formContainer);
-    formContainer.appendChild(div)
-    div.appendChild(heading);
-    div.appendChild(exitButton);
-    formContainer.appendChild(title);
-    formContainer.appendChild(author);
-    formContainer.appendChild(pages);
-    formContainer.appendChild(labelStatus);
-    formContainer.appendChild(status);
-    formContainer.appendChild(submit);  
-    
-    exitButton.onclick = function() {
-        formContainer.remove();
-        closeModal();
-    };
-}
-
 const overlayModal = () => {
     overlay.classList.add('overlay');
-    createFormModal()
 }
 const closeModal = () => {
     overlay.classList.remove('overlay');
 }
-function submit(e) {
+function submit1(e) {
     if (e.key == 'Enter') {
         e.preventDefault()
     handleKeyboardInput()
@@ -122,8 +177,4 @@ const handleKeyboardInput = () => {
     openAccountModal()
 }
 
-const createForm = () => {
-    overlayModal();
-}
-
-username.onkeydown = submit;
+username.onkeydown = submit1;
